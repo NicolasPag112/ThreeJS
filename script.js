8fa46b86efffe894cf1a66b1e6e3bc77b0e8e644
NicolasPag112 <62311522+NicolasPag112@users.noreply.github.com>
2025-06-26 17:19:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Inicializando a cena, a câmera e o renderizadorAdd commentMore actions
+// Inicializando a cena, a câmera e o renderizador
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });

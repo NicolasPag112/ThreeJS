711bd6417b58f619a1b5c95a5d46ed7630e93859
NicolasPag112 <62311522+NicolasPag112@users.noreply.github.com>
2025-06-26 17:16:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -88,6 +88,6 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-});Add commentMore actions
+});
 
 animate();
